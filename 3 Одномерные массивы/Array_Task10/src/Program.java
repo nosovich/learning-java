2178346f7b1a0a115d4dc1e971474379e3fac913
Roma <nosovich1501@gmail.com>
2021-07-02 18:44:03 +0200

    Tasks 9-10

---
+++ 3 Одномерные массивы/Array_Task10/src/Program.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+//    Пользователь вводит с клавиатуры число большее 3, которое сохраняется в
+//    переменную n. Если пользователь ввёл не подходящее число, то программа
+//    должна выдать соответствующее сообщение. Создать массив из n случайных
+//    целых чисел из отрезка [0;n] и вывести его на экран. Создать второй массив
+//    только из чётных элементов первого массива, если они там есть, и вывести его
+//    на экран.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое положительное число больше чем три.");
+        int number = scanner.nextInt();
+        if (number <= 0 || number % 1 != 0 || number <= 3) {
+            System.out.println("Вы ввели неподходящее число. Пожалуйста, попытайтесь снова.");
+            return;
+        }
+        int[] array1 = new int[number];
+        Random random = new Random();
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = random.nextInt(number);
+        }
+        for (int element : array1) {
+            System.out.print(element + " ");
+        }
+        System.out.println("");
+        System.out.println("Вариант 1 наполнения второго массива: ");
+        int[] array2 = new int[array1.length];
+        int currentIndex = 0;
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] % 2 == 0 && array1[i] != 0) {
+                array2[currentIndex] = array1[i];
+                currentIndex++;
+            }
+        }
+        for (int element : array2) {
+            System.out.print(element + " ");
+        }
+        System.out.println("");
+        System.out.println("Вариант 2 наполнения второго массива: ");
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] % 2 == 0) {
+                array2[i] = array1[i];
+            }
+        }
+        for (int element : array2) {
+            System.out.print(element + " ");
+        }
+    }
+}
+
---
+++ 3 Одномерные массивы/Array_Task9/src/Program.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+//    Пользователь должен указать с клавиатуры положительное число, а
+//    программа должна создать массив указанного размера из случайных целых
+//    чисел из [0;15] и вывести его на экран в строку. После этого программа должна
+//    определить и сообщить пользователю о том, сумма какой половины массива
+//    больше: левой или правой, либо сообщить, что эти суммы модулей равны. Если
+//    пользователь введёт неподходящее число, то выдать соответствующее
+//    сообщение
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое положительное число больше единицы.");
+        int number = scanner.nextInt();
+        if (number <= 0 || number % 1 != 0 || number == 1) {
+            System.out.println("Вы ввели неподходящее число. Пожалуйста, попытайтесь снова.");
+            return;
+        }
+        int[] array = new int[number];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(15);
+        }
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println("");
+        int leftPart = 0;
+        int rightPart = 0;
+        int lastLeftPartId = array.length / 2 - 1;
+        int firstRightPartId = lastLeftPartId + 1;
+        if (array.length % 2 != 0){
+            firstRightPartId++;
+        }
+        for (int i = 0; i <= lastLeftPartId; i++){
+            leftPart += array[i];
+        }
+        for (int i = firstRightPartId; i < array.length; i++){
+
+            rightPart += array[i];
+        }
+        System.out.println("Сумма левой половины массива: " + leftPart);
+        System.out.println("Сумма правой половины массива: " + rightPart);
+        if(leftPart > rightPart){
+            System.out.println("Сумма левой половины массива больше.");
+        }
+        else if (rightPart > leftPart){
+            System.out.println("Сумма правой половины массива больше.");
+        }
+        else {
+            System.out.println("Суммы двух половин массива равны.");
+        }
+    }
+}
